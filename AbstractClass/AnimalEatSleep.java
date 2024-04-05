4cb6ced9d1a4116f4c839b66ed46a6e0324a666b
itsmeakhilsr <akhilsr628@gmail.com>
2024-04-05 09:46:35 +0530

    Final Commit

--- AbstractClass/AnimalEatSleep.java
+++ AbstractClass/AnimalEatSleep.java
@@ -1,5 +1,5 @@
 //4. Write a Java program to create an abstract class Animal with abstract methods eat() and sleep().
-//Create subclasses Lion, Tiger, and Deer that extend the Animal class and implement the eat() and sleep() methods differently based on their specific behavior.
+//Create subclasses Lion, Tiger, and Deer that extend the Animal class and implement the eat() and sleep() methods differently based on their specific behavior .
 package AbstractClass;
 
 abstract class Animal {
